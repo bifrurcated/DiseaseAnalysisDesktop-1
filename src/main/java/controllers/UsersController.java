1ca7712f5f7780b00f814cf3d8ecb7e3f789af9b
bifrurcated <55530887+bifrurcated@users.noreply.github.com>
2021-03-10 22:42:44 +1000

    Изменил клас подключения к БД и новая версия junit

--- src/main/java/controllers/UsersController.java
+++ src/main/java/controllers/UsersController.java
@@ -1,42 +1,57 @@
 package controllers;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-import dao.UserDAO;
+import dao.DatabaseRepository;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import models.User;
 
 
-public class UsersController {
-    private final UserDAO userDAO;
+public class UsersController implements Initializable {
+    private final DatabaseRepository dataBase;
 
-    @FXML private ResourceBundle resources;
-    @FXML private URL location;
     @FXML private Pane pane;
     @FXML private ListView<String> listNames;
 
     private ObservableList<String> names;
 
-    @FXML
-    void initialize() {
-        assert pane != null : "fx:id=\"textField\" was not injected: check your FXML file 'sample.fxml'.";
-        assert listNames != null : "fx:id=\"listNames\" was not injected: check your FXML file 'sample.fxml'.";
-        listNames.setItems(names);
+    public UsersController(){
+        dataBase = new DatabaseRepository();
     }
 
-    public UsersController(){
-        this.userDAO = new UserDAO();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        assert pane != null : "fx:id=\"textField\" was not injected: check your FXML file 'sample.fxml'.";
+        assert listNames != null : "fx:id=\"listNames\" was not injected: check your FXML file 'sample.fxml'.";
         names = FXCollections.observableArrayList();
+        listNames.setItems(names);
+        if(dataBase.connect()){
+            System.out.println("База данных подключена");
+        }else{
+            System.out.println("Нет подключения к базе данных");
+        }
     }
+
     public void onClick(MouseEvent mouseEvent) {
-        userDAO.showAll().stream()
-                .filter(user -> !names.contains(user.getName()))
-                .forEach(user -> names.add(user.getName()));
+        try{
+            ResultSet resultSet = dataBase.select("SELECT * FROM users;");
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                if(!names.contains(name)){
+                    names.add(name);
+                }
+            }
+            resultSet.getStatement().close();
+            resultSet.close();
+        } catch (SQLException sqlException){
+            sqlException.printStackTrace();
+        }
     }
 }
