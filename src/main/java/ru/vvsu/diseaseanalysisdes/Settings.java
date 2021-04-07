31839890b15a50370902e71e94d7813673ab89d6
bifrurcated <55530887+bifrurcated@users.noreply.github.com>
2021-04-07 22:25:02 +1000

    fixed how sizer

--- src/main/java/ru/vvsu/diseaseanalysisdes/Settings.java
+++ src/main/java/ru/vvsu/diseaseanalysisdes/Settings.java
@@ -3,7 +3,7 @@ package ru.vvsu.diseaseanalysisdes;
 public class Settings {
 
     public static final String DIR_NAME = "DiseaseAnalysis";
-    public static final String DB_PATH_SOURCE = "/db/precedent.db";
+    public static final String DB_PATH_SOURCE = "/files/db/precedent.db";
     public static final String DB_FILE_NAME = "precedent.db";
 
 }
