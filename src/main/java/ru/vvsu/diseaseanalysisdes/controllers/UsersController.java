bb63d35f430b5018b0546366c170641b2975cb42
bifrurcated <55530887+bifrurcated@users.noreply.github.com>
2021-04-16 03:35:02 +1000

    change tableView

--- src/main/java/ru/vvsu/diseaseanalysisdes/controllers/UsersController.java
+++ src/main/java/ru/vvsu/diseaseanalysisdes/controllers/UsersController.java
@@ -1,17 +1,14 @@
 package ru.vvsu.diseaseanalysisdes.controllers;
 
 import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Toggle;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import ru.vvsu.diseaseanalysisdes.helpers.FileHelper;
 import ru.vvsu.diseaseanalysisdes.managers.SQLiteManager;
@@ -28,14 +25,18 @@ import java.util.*;
 
 public class UsersController implements Initializable {
     private final SQLiteManager dataBase;
+    @FXML private TableView<Human> tableViewEnterData;
+    @FXML private TableView<Human> tableViewResultSearch;
 
     @FXML private ToggleGroup genderToggleGroup;
 
-    //private ObservableList<String> names = FXCollections.observableArrayList();
+    private ObservableList<Human> enterDataList;
+    private ObservableList<Human> resultSearchList;
     private List<Human> humanList;
     private Human user;
     private Algo algo;
     private Map<String, Double> probabilityMap;
+    private Map<String, String> namesColumns;
 
     public UsersController(){
         dataBase = new SQLiteManager();
@@ -43,10 +44,68 @@ public class UsersController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        namesColumns = new LinkedHashMap<>(40);
+        namesColumns.put("sex","Пол");
+        namesColumns.put("age","Возраст");
+        namesColumns.put("height","Рост");
+        namesColumns.put("weight","Вес");
+        namesColumns.put("freq_meat","Как часто вы употребляете мясо?");
+        namesColumns.put("freq_fish","Как часто вы употребляете рыбу?");
+        namesColumns.put("freq_vegatables","Как часто вы употребляете свежие овощи?");
+        namesColumns.put("freq_sweets","Как часто вы употребляете сладости?");
+        namesColumns.put("freq_cottage_cheese","Как часто вы употребляете творог?");
+        namesColumns.put("freq_cheese","Как часто вы употребляете сыр?");
+        namesColumns.put("exercise_stress_on_work","Уровень физ. нагрузки во время работы");
+        namesColumns.put("walk","Сколько в среднем минут в день ходите пешком");
+        namesColumns.put("exercise_stress","Сколько раз в неделю физ. нагрузка?");
+        namesColumns.put("cigarettes","Сколько в среднем сигарет вы выкуриваете в день?");
+        namesColumns.put("headaches","Головные боли бывают?");
+        namesColumns.put("sleep","Длительность ежедневного сна?");
+        namesColumns.put("abstinence_from_sleep","Как часто вы испытывали трудности в том, чтобы воздерживаться от засыпания когда ситуация этого требует?");
+        namesColumns.put("fall_asleep","Насколько часто Вам было трудно заснуть в течение 30 мин после того как вы легли в постель?");
+        namesColumns.put("restless","Я неусидчив");
+        namesColumns.put("waist","Окружность талии");
+        namesColumns.put("hips","Окружность бедер");
+        namesColumns.put("average_systolic","Среднее систолическое артериальное давление");
+        namesColumns.put("average_diastolic","Среднее диастолическое артериальное давление");
+        namesColumns.put("average_heart_rate","Средняя частота сердечных сокращений");
+        namesColumns.put("total_cholesterol","Общий холестерин");
+        namesColumns.put("hdl","ЛПВП");
+        namesColumns.put("lpa","ЛП(a)");
+        namesColumns.put("apob","APOB");
+        namesColumns.put("glucose","Глюкоза");
+        namesColumns.put("creatinine","Креатинин");
+        namesColumns.put("uric_acid","Мочевая кислота");
+        namesColumns.put("crp","C-реактивный белок");
+        namesColumns.put("insulin","Инсулин");
+        namesColumns.put("tsh","ТТГ");
+        namesColumns.put("probnp","PROBNP");
+
+        enterDataList = FXCollections.observableArrayList();
+        resultSearchList = FXCollections.observableArrayList();
+        tableViewEnterData.setItems(enterDataList);
+        tableViewResultSearch.setItems(resultSearchList);
+
+        namesColumns.forEach((key,val) -> {
+            TableColumn<Human,String> tableColumnEnterData = new TableColumn<>(val);
+            tableColumnEnterData.setCellValueFactory(new PropertyValueFactory<>(key));
+            tableViewEnterData.getColumns().add(tableColumnEnterData);
+            TableColumn<Human,String> tableColumnResultSearch = new TableColumn<>(val);
+            tableColumnResultSearch.setCellValueFactory(new PropertyValueFactory<>(key));
+            tableViewResultSearch.getColumns().add(tableColumnResultSearch);
+            if(key.equals("weight") || key.equals("fall_asleep") || key.equals("hips")){
+                tableColumnEnterData = new TableColumn<>(".........");
+                tableViewEnterData.getColumns().add(tableColumnEnterData);
+                tableColumnResultSearch = new TableColumn<>(".........");
+                tableViewResultSearch.getColumns().add(tableColumnResultSearch);
+            }
+        });
+
         humanList = new ArrayList<>();
         probabilityMap = new HashMap<>(9);
         algo = new Algo();
         user = new Human();
+
         //для теста
         user.height = "160";
         user.age = "28";
@@ -149,5 +208,54 @@ public class UsersController implements Initializable {
             return FileHelper.deserialize(configFile.getName());
         }
         return null;
+    }
+
+    public void handleBtnContinue(ActionEvent actionEvent) {
+        if(!enterDataList.isEmpty()){ enterDataList.remove(0); }
+        enterDataList.add(user);
+
+        if(!resultSearchList.isEmpty()){ resultSearchList.remove(0, resultSearchList.size()); }
+
+        algo.setPercent(5); // задаём начальный процент выборки
+        System.out.println(algo.getIndexMassBody(user.height, user.weight));
+        Runnable searchEqualUser = () -> {
+            int count = 0;
+            while (count < 1){
+                StringBuilder sb = algo.getQuerySelections(user); System.out.println(sb);
+                try{
+                    ResultSet resultSet = dataBase.getResultSet("SELECT * FROM med_card where "+sb+";");
+                    while (resultSet.next()) {
+                        Human human = new Human();
+                        Arrays.stream(human.getClass().getFields()).forEach(val -> {
+                            try {
+                                val.set(human,resultSet.getString(val.getName()));
+                            } catch (IllegalAccessException | SQLException e) {
+                                e.printStackTrace();
+                            }
+                        }); // каждой public переменной присваиваем значение из выборки
+                        humanList.add(human);
+                        resultSearchList.add(human);
+                        count++;
+                    }
+                    if(count < 1){
+                        algo.setPercent(algo.getPercent()+1); //увеличиваем процент выборки
+                    }
+                    if(count > 2){
+                        probabilityMap = algo.getProbabilityHealthy(humanList);
+                        System.out.println(probabilityMap);
+                    }
+                    resultSet.close();
+                    resultSet.getStatement().close();
+
+                } catch (SQLException sqlException){
+                    sqlException.printStackTrace();
+                }
+            }
+            System.out.println("percent = "+algo.getPercent());
+
+        };
+        Thread thread = new Thread(searchEqualUser);
+        thread.setDaemon(true);
+        thread.start();
     }
 }
--- src/main/java/ru/vvsu/diseaseanalysisdes/models/Human.java
+++ src/main/java/ru/vvsu/diseaseanalysisdes/models/Human.java
@@ -56,4 +56,356 @@ public class Human implements Serializable {
 
 
     public Human(){ }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getWaist() {
+        return waist;
+    }
+
+    public void setWaist(String waist) {
+        this.waist = waist;
+    }
+
+    public String getHips() {
+        return hips;
+    }
+
+    public void setHips(String hips) {
+        this.hips = hips;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getWalk() {
+        return walk;
+    }
+
+    public void setWalk(String walk) {
+        this.walk = walk;
+    }
+
+    public String getCigarettes() {
+        return cigarettes;
+    }
+
+    public void setCigarettes(String cigarettes) {
+        this.cigarettes = cigarettes;
+    }
+
+    public String getAverage_systolic() {
+        return average_systolic;
+    }
+
+    public void setAverage_systolic(String average_systolic) {
+        this.average_systolic = average_systolic;
+    }
+
+    public String getAverage_diastolic() {
+        return average_diastolic;
+    }
+
+    public void setAverage_diastolic(String average_diastolic) {
+        this.average_diastolic = average_diastolic;
+    }
+
+    public String getAverage_heart_rate() {
+        return average_heart_rate;
+    }
+
+    public void setAverage_heart_rate(String average_heart_rate) {
+        this.average_heart_rate = average_heart_rate;
+    }
+
+    public String getTotal_cholesterol() {
+        return total_cholesterol;
+    }
+
+    public void setTotal_cholesterol(String total_cholesterol) {
+        this.total_cholesterol = total_cholesterol;
+    }
+
+    public String getHdl() {
+        return hdl;
+    }
+
+    public void setHdl(String hdl) {
+        this.hdl = hdl;
+    }
+
+    public String getLpa() {
+        return lpa;
+    }
+
+    public void setLpa(String lpa) {
+        this.lpa = lpa;
+    }
+
+    public String getApob() {
+        return apob;
+    }
+
+    public void setApob(String apob) {
+        this.apob = apob;
+    }
+
+    public String getGlucose() {
+        return glucose;
+    }
+
+    public void setGlucose(String glucose) {
+        this.glucose = glucose;
+    }
+
+    public String getCreatinine() {
+        return creatinine;
+    }
+
+    public void setCreatinine(String creatinine) {
+        this.creatinine = creatinine;
+    }
+
+    public String getUric_acid() {
+        return uric_acid;
+    }
+
+    public void setUric_acid(String uric_acid) {
+        this.uric_acid = uric_acid;
+    }
+
+    public String getCrp() {
+        return crp;
+    }
+
+    public void setCrp(String crp) {
+        this.crp = crp;
+    }
+
+    public String getInsulin() {
+        return insulin;
+    }
+
+    public void setInsulin(String insulin) {
+        this.insulin = insulin;
+    }
+
+    public String getTsh() {
+        return tsh;
+    }
+
+    public void setTsh(String tsh) {
+        this.tsh = tsh;
+    }
+
+    public String getProbnp() {
+        return probnp;
+    }
+
+    public void setProbnp(String probnp) {
+        this.probnp = probnp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getFreq_meat() {
+        return freq_meat;
+    }
+
+    public void setFreq_meat(String freq_meat) {
+        this.freq_meat = freq_meat;
+    }
+
+    public String getFreq_fish() {
+        return freq_fish;
+    }
+
+    public void setFreq_fish(String freq_fish) {
+        this.freq_fish = freq_fish;
+    }
+
+    public String getFreq_vegatables() {
+        return freq_vegatables;
+    }
+
+    public void setFreq_vegatables(String freq_vegatables) {
+        this.freq_vegatables = freq_vegatables;
+    }
+
+    public String getFreq_sweets() {
+        return freq_sweets;
+    }
+
+    public void setFreq_sweets(String freq_sweets) {
+        this.freq_sweets = freq_sweets;
+    }
+
+    public String getFreq_cottage_cheese() {
+        return freq_cottage_cheese;
+    }
+
+    public void setFreq_cottage_cheese(String freq_cottage_cheese) {
+        this.freq_cottage_cheese = freq_cottage_cheese;
+    }
+
+    public String getFreq_cheese() {
+        return freq_cheese;
+    }
+
+    public void setFreq_cheese(String freq_cheese) {
+        this.freq_cheese = freq_cheese;
+    }
+
+    public String getExercise_stress_on_work() {
+        return exercise_stress_on_work;
+    }
+
+    public void setExercise_stress_on_work(String exercise_stress_on_work) {
+        this.exercise_stress_on_work = exercise_stress_on_work;
+    }
+
+    public String getExercise_stress() {
+        return exercise_stress;
+    }
+
+    public void setExercise_stress(String exercise_stress) {
+        this.exercise_stress = exercise_stress;
+    }
+
+    public String getSleep() {
+        return sleep;
+    }
+
+    public void setSleep(String sleep) {
+        this.sleep = sleep;
+    }
+
+    public String getFall_asleep() {
+        return fall_asleep;
+    }
+
+    public void setFall_asleep(String fall_asleep) {
+        this.fall_asleep = fall_asleep;
+    }
+
+    public String getAbstinence_from_sleep() {
+        return abstinence_from_sleep;
+    }
+
+    public void setAbstinence_from_sleep(String abstinence_from_sleep) {
+        this.abstinence_from_sleep = abstinence_from_sleep;
+    }
+
+    public String getOsteochondrosis() {
+        return osteochondrosis;
+    }
+
+    public void setOsteochondrosis(String osteochondrosis) {
+        this.osteochondrosis = osteochondrosis;
+    }
+
+    public String getRheumatoid_arthritis() {
+        return rheumatoid_arthritis;
+    }
+
+    public void setRheumatoid_arthritis(String rheumatoid_arthritis) {
+        this.rheumatoid_arthritis = rheumatoid_arthritis;
+    }
+
+    public String getStroke() {
+        return stroke;
+    }
+
+    public void setStroke(String stroke) {
+        this.stroke = stroke;
+    }
+
+    public String getMyocardial_infarction() {
+        return myocardial_infarction;
+    }
+
+    public void setMyocardial_infarction(String myocardial_infarction) {
+        this.myocardial_infarction = myocardial_infarction;
+    }
+
+    public String getCoronary_heart_disease() {
+        return coronary_heart_disease;
+    }
+
+    public void setCoronary_heart_disease(String coronary_heart_disease) {
+        this.coronary_heart_disease = coronary_heart_disease;
+    }
+
+    public String getArrhythmia() {
+        return arrhythmia;
+    }
+
+    public void setArrhythmia(String arrhythmia) {
+        this.arrhythmia = arrhythmia;
+    }
+
+    public String getKidney_disease() {
+        return kidney_disease;
+    }
+
+    public void setKidney_disease(String kidney_disease) {
+        this.kidney_disease = kidney_disease;
+    }
+
+    public String getThyroid_disease() {
+        return thyroid_disease;
+    }
+
+    public void setThyroid_disease(String thyroid_disease) {
+        this.thyroid_disease = thyroid_disease;
+    }
+
+    public String getHeadaches() {
+        return headaches;
+    }
+
+    public void setHeadaches(String headaches) {
+        this.headaches = headaches;
+    }
+
+    public String getRestless() {
+        return restless;
+    }
+
+    public void setRestless(String restless) {
+        this.restless = restless;
+    }
 }
